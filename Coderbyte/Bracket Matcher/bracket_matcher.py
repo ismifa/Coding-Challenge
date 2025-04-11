3625301d3dff6001667bce15988a599bd8b98f95
Ismi Yasifa <37787890+ismifa@users.noreply.github.com>
2025-04-11 21:12:01 +0700

    Create bracket_matcher.py

---
+++ Coderbyte/Bracket Matcher/bracket_matcher.py
@@ -0,0 +1,15 @@
+def BracketMatcher(strParam):
+
+  bracket = []
+
+  for i in strParam:
+    if i == '(':
+      bracket.append(i)
+    elif i == ')':
+      if not bracket:
+        return 0
+      bracket.pop() #remove the last opening parenthesis that has a matching closing one
+      
+  return 1
+
+print(BracketMatcher(input()))
